8e4f58d1340dc2e379d404f6b885bc2043fe0c4e
Shaf-Shafiq12 <i201864@nu.edu.pk>
2023-04-18 17:29:38 +0500

    everthing working here

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import {BrowserRouter , Routes , Route} from 'react-router-dom';
+import Home from './pages/home';
+import NotFound from './pages/notFound';
+import {CssBaseline , ThemeProvider} from '@mui/material';
+import { theme } from './theme';
 
-function App() {
+const App = () => {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    < >
+      <ThemeProvider theme={theme}>
+           <CssBaseline/>
+              <BrowserRouter>
 
+                 <Routes>  
+
+                  <Route path="/" element={<Home/>}/>
+           
+                  <Route path="*" element={<NotFound/>}/>
+            
+              </Routes>
+              </BrowserRouter>
+           <CssBaseline/>
+        </ThemeProvider>
+
+    </>
+  );
+
+}
 export default App;
